3ad0f1371ecb154800bd28d6c344285b5181aa49
Ronald Pereira Evangelista <Ronald.Evangelista@Aluno.ifsp.edu.br>
2024-12-30 23:47:25 -0300

    Adicionar arquivos de projeto.

---
+++ SWII6P2Verifications/Verifications.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWII6P2Verifications
+{
+    public static class Verifications
+    {
+        public static bool IsProductFull(string name, double price)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+
+            if (price == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsUserFull(string name, string password)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
